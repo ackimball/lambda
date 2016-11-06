bdef48738546305d4b327ae1f9e6f11d71fd06eb
Wyatt Cooper <wcooper17@cmc.edu>
2016-11-06 13:50:44 -0800

    now can run programs, evalLam still doesn't quite work

--- hw06.hs
+++ hw06.hs
@@ -22,7 +22,7 @@ data LamExp =
     Var VarName
   | App LamExp LamExp
   | Lam VarName LamExp
-  deriving (Show,Eq)
+  deriving Eq
 
 data Stmt =
       Let VarName LamExp
@@ -32,15 +32,15 @@ data Stmt =
 
 
 
--- instance Show LamExp where
---   show = show' 0 where
---     show' _ (Var v) = v
---     show' z (App la1 la2)
---      | z < 1 = show' 1 la1 ++ " " ++ show' 1 la2
---      | otherwise = "(" ++ show' 1 la1 ++ " " ++ show' 1 la2 ++ ")"
---     show' z (Lam x la)
---      | z < 1 = "lambda " ++ show' 1 (Var x) ++ ". " ++ show' 1 la
---      | otherwise = "(" ++ "lambda " ++ show' 1 (Var x) ++ ". " ++ show' 1 la ++ ")"
+instance Show LamExp where
+  show = show' 0 where
+    show' _ (Var v) = v
+    show' z (App la1 la2)
+     | z < 1 = show' 1 la1 ++ " " ++ show' 1 la2
+     | otherwise = "(" ++ show' 1 la1 ++ " " ++ show' 1 la2 ++ ")"
+    show' z (Lam x la)
+     | z < 1 = "lambda " ++ show' 1 (Var x) ++ ". " ++ show' 1 la
+     | otherwise = "(" ++ "lambda " ++ show' 1 (Var x) ++ ". " ++ show' 1 la ++ ")"
 
 
 test1 = Var "x"
@@ -96,7 +96,7 @@ ws :: Parser ()
 ws = void $ many $ oneOf " \n\t"
 
 lexp ::Parser LamExp
-lexp = ws *> (chainl1 (try lamP <|> try varP <|> (parens lexp)) op)
+lexp = ws *> (chainl1 (lamP <|> varP <|> (parens lexp)) op)
 
 varP :: Parser LamExp
 varP =  Var <$> (ws *> var)
@@ -105,13 +105,13 @@ firstChar = satisfy (\a -> isLetter a || a == '_')
 nonFirstChar = satisfy (\a -> isDigit a || isLetter a || a == '_')
 
 lamP :: Parser LamExp
-lamP = try oneArg <|> multArgs
+lamP = try oneArg <|> try multArgs
         where 
           oneArg = Lam <$> (((kw "lambda") *> var <* dot)) <*> lexp
           multArgs = Lam <$> ((kw "lambda") *> var) <*> lamP2
 
 lamP2 :: Parser LamExp
-lamP2 = try moreArgs <|> try lastArg
+lamP2 = try moreArgs <|> lastArg
         where
           lastArg = Lam <$> (var <* dot) <*> lexp
           moreArgs = Lam <$> var <*> lamP2
@@ -143,9 +143,9 @@ stmtParser = try expCase <|> try letCase
 
 --Substitution
 subst :: LamExp -> VarName -> LamExp -> LamExp
-subst (Var y) x e = if y == x then e else (Var y)
+subst (Var y) x e = if (Var y) == e then (Var y) else (if (x == y) then e else (Var y))
 subst (App e1 e2) x e3 = App (subst e1 x e2) (subst e2 x e3)
-subst (Lam y e1) x e2 = if y == x then Lam x e1 else Lam y (subst e1 x e2)
+subst (Lam y e1) x e2 = if y == x then e1 else (subst e1 x e2)
 
 -- EXAMPLE of how interpreter should work
 -- If we parse test2, we get: 
@@ -165,23 +165,34 @@ subst (Lam y e1) x e2 = if y == x then Lam x e1 else Lam y (subst e1 x e2)
 -- So this needs to use substitution somehow
 evalLam :: Store -> LamExp -> LamExp
 evalLam st (Var x) = findWithDefault (Var "x") x st
-evalLam st (Lam x la) = undefined
-evalLam st (App l1 l2) = undefined
+evalLam st (Lam x la) = Lam x (evalLam st la)
+evalLam st (App l1 l2) = (evalLam st l1)                
+
 
 -- Interpreter for Statements
 evalStmt :: Store -> Stmt -> Store
 evalStmt st (Let x l) = Map.insert x (evalLam st l) st
-evalStmt st (Exp l) = undefined --I had "evalLam st l" but leads to type error. any other ideas?
+evalStmt st (Exp l) = st
 evalStmt st (Seq s1 s2) = (evalStmt (evalStmt st s1) s2)
 
+evalStmt2 :: [LamExp] -> Stmt -> [LamExp]
+evalStmt2 l (Let x a) = l
+evalStmt2 l (Exp a) = (a:l)
+evalStmt2 l (Seq s1 s2) = (evalStmt2 (evalStmt2 l s1) s2)
 
+evalAll :: [LamExp] -> Store -> String
+evalAll [] _ = "Running program"
+evalAll (l:ls) st = (evalAll ls st) ++ "\n" ++ (show (evalLam st l)) ++"\n"
 
 --Checking for free variables
 --(will be used with the -c flag)
 fv :: LamExp -> Set VarName
 fv (Var x) = Set.singleton x
 fv (App e1 e2) = Set.union (fv e1) (fv e2)
-fv (Lam x e) = Set.difference (fv e) (Set.singleton x)  
+fv (Lam x e) = Set.difference (fv e) (Set.singleton x)
+
+
+
 
 --e is closed IFF fv(e) = empty set
 isClosed :: LamExp -> Bool
@@ -191,10 +202,24 @@ main :: IO ()
 main = do
     a <- getArgs
     case a of
-      [str] -> parseFromFile program str >>= either print print
+      [str] -> do 
+            prog <- (parseFromFile program str)
+            case prog of
+                  Right e1 -> let stores = (evalStmt s e1) 
+                                  lams = (evalStmt2 g e1) in 
+                                  putStr (evalAll lams stores)
+                  Left e2  -> error (show e2)
+                  where s = Map.empty
+                        g = []
+
       _ -> do 
              input <- getContents 
              case (regularParse program input) of
-                  Right e1 -> putStr (show e1)
+                  Right e1 -> let s = (evalStmt s e1) 
+                                  g = (evalStmt2 g e1) in 
+                                  putStr (evalAll g s)
                   Left e2  -> error (show e2)
+                  where s = Map.empty
+                        g = []
+
 
